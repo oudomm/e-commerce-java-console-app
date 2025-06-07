83b8eb2566801948c2926888c614132686da1e53
oudomm <oudomphoem@gmail.com>
2025-06-07 23:39:50 +0700

    Update

---
+++ src/main/java/model/entity/Order.java
@@ -0,0 +1,15 @@
+package model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+    private Integer userId;
+    private List<Integer> productIds;
+}
---
+++ src/main/java/model/entity/Product.java
@@ -0,0 +1,10 @@
+package model.entity;
+
+public class Product {
+    private Integer id;
+    private String pUuid;
+    private String pName;
+    private Double price;
+    private Integer qty;
+    private Boolean isDeleted;
+}
