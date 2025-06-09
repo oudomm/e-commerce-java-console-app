f25971aa0e64f5416df9d159f449a8e6f6644934
Houy-G <https://github.com/Houy-G>
2025-06-09 07:52:13 +0700

    Houy

--- src/main/java/model/service/UserService.java
+++ src/main/java/model/service/UserService.java
@@ -1,4 +1,16 @@
 package model.service;
 
+import model.dto.UserCreateDto;
+import model.dto.UserResponseDto;
+import model.dto.UserloginDto;
+
+import java.util.List;
+import java.util.Map;
+
 public interface UserService {
+    List<UserResponseDto> getAllUser();
+    String  signUp(UserCreateDto create);
+    String login(UserloginDto login);
+    void saveUserLog(Map<String,String> map);
+    UserloginDto autoLogin();
 }
--- src/main/java/model/service/UserServiceImpl.java
+++ src/main/java/model/service/UserServiceImpl.java
@@ -1,4 +1,83 @@
 package model.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mapper.UserMapper;
+import model.dto.UserCreateDto;
+import model.dto.UserResponseDto;
+import model.dto.UserloginDto;
+import model.entity.User;
+import model.repository.UserRepository;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository = new UserRepository();
+    private static final String USERS_FILE = "src/main/java/model/file/userlogs/users.json";
+    private static final ObjectMapper mapper = new ObjectMapper();
+    @Override
+    public List<UserResponseDto> getAllUser() {
+        List<UserResponseDto> userResponseDtos = new ArrayList<>();
+        userRepository.findAll().forEach(e->userResponseDtos.add(UserMapper.mapFromUserToUserResponseDto(e)));
+        return userResponseDtos;
+    }
+
+    @Override
+    public String signUp(UserCreateDto create) {
+        User user = userRepository.findUserByEmail(create.email());
+        if (user == null){
+            userRepository.save(UserMapper.mapFromCreateUseToUser(create));
+            return user.getUUuid();
+        }else System.out.println("User is already exist");
+        return null;
+    }
+    @Override
+    public String login(UserloginDto login) {
+        User user = userRepository.findUserByEmail(login.email());
+        if (user != null){
+            if (user.getEmail().equals(login.email())){
+                if(user.getPassword() == login.password()){
+                    user.setStatus(true);
+                    Map<String,String > map = new TreeMap<>();
+                    map.put("email", user.getEmail());
+                    map.put("password",user.getPassword());
+                    map.put("status",user.getStatus().toString());
+                    saveUserLog(map);
+                    return user.getUUuid();
+                }
+            }
+        }
+        return null;
+    }
+    @Override
+    public void saveUserLog(Map<String,String> map) {
+        File file =new File(USERS_FILE);
+        try{
+            file.createNewFile();
+            mapper.writeValue(file,map);
+        }catch (Exception e){
+            System.out.println(
+                    e.getMessage()
+            );
+        }
+    }
+    @Override
+    public UserloginDto autoLogin() {
+        Map<String ,String > user = readDate();
+        return new UserloginDto(user.get("email"),user.get("password"),Boolean.valueOf(user.get("status")));
+    }
+    private Map<String ,String > readDate(){
+        try{
+            return mapper.readValue(USERS_FILE, new TypeReference<Map<String, String>>() {});
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
